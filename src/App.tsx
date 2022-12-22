d961256ab1b07e8851a5c3930ecd6889b99886d5
odumade <104792577+odumade@users.noreply.github.com>
2022-12-22 12:51:05 +0100

    complete react, tailwind, typescript, heroicon, folders and configuration set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/scenes/navbar";
+
+function App() {
+  return (
+    <div className="app bg-gray-20">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+type Props = {}
+
+const NavBar = (props: Props) => {
+    return (
+        <div>
+            NavBar
+        </div>
+    );
+};
+
+export default NavBar;
